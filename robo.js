00c3dc5b1a1dd2e7070285a96392e3b38de72611
mariosanguineti <mario.sanguineti@gmail.com>
2022-07-13 17:15:30 +1000

    Added, rectangle size and obstacles

--- robo.js
+++ robo.js
@@ -1,49 +1,37 @@
-/*
-Instructions
-1. You are required to simulate a toy robot moving on a square tabletop, of dimensions 5 units x 5 units.
-2. There are no other obstructions on the table surface. The robot is free to roam around the surface of the table, but must be prevented from falling to destruction.
-3. Any movement that would result in the robot falling from the table must be prevented, however further valid movement commands must still be allowed.
-4. All commands should be discarded until a valid place command has been executed.
-5. The solution can be written in Javascript, Typescript, C#, C++ or Python. Ideally, it will be written in your strongest language.
-6. The UI can be provided via CLI, however you are free to expand on this.
-7. Keep it simple, keep it DRY, but don’t over complicate or over engineer, comment and test as much as possible.
-8. Include a README file with instructions on how to build/compile your solution and how to run it.
-9. Share your code via a public GitHub repository, git bundle or zip file.
-10. We like to see how you work, not just the end result.
-Commands
-1.All commands should provide output indicating whether or not they succeeded.
-PLACE X,Y,DIRECTION
-2. X and Y are integers that indicate a location on the tabletop.
-DIRECTION is a string indicating which direction the robot should face. It it one of the four cardinal directions: NORTH, EAST, SOUTH or WEST.
-3. MOVE
-Instructs the robot to move 1 square in the direction it is facing.
-4. LEFT
-Instructs the robot to rotate 90° anticlockwise/counterclockwise.
-5.RIGHT
-Instructs the robot to rotate 90° clockwise.
-6.REPORT
-Outputs the robot's current location on the tabletop and the direction it is facing.
-*/
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Robot 9th-July-2020 / Mario Barthlomew Sanguineti
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
-var rSquareSize = 75;
-var sCurrentDir = "East";
-var iTableTopSize = 5;
-var xCurrent = rSquareSize / 2;
-var yCurrent = rSquareSize / 2;
-var rBoundary = iTableTopSize * rSquareSize;
+var rSquareSizeX = 100;                         // px width of square
+var rSquareSizeY = 50;                          // px height of square
+var sCurrentDir = "East";                       // initial location robot is facing
+var iTableTopSize = 8;                          // Grid size 8x8
+var rBoundaryX = iTableTopSize * rSquareSizeX;  // Total width of play area for robot
+var rBoundaryY = iTableTopSize * rSquareSizeY;  // Total height of play area for robot
+var iOffBound  = 0;                             // boundar between play wall and canvas wall
+var xCurrent = rSquareSizeX / 2 + iOffBound/2;  // initial default location X of robot
+var yCurrent = rSquareSizeY / 2 + iOffBound/2;  // initial default location Y of robot
+var clickMode='robot';                          // initial default placement of onclick is to place robot
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+// obstacle setup
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+var Obstacle = new Array(iTableTopSize+1);
+for(var i = 0; i< Obstacle.length; i++){
+   Obstacle[i] = new Array(iTableTopSize+1);
+};
+ClearObstacles();
+DoPlaceRobot();
+Obstacle[iTableTopSize/2][iTableTopSize/2]=1;    // initial placement of obstacle rougthly at middle of play area
+Obstacle[1][1]=2;                                // initial placement of robot
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Main
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 var canvas = document.getElementById("myCanvas");
-canvas.width = rBoundary + 3;
-canvas.height = rBoundary + 3;
-
+canvas.width  = rBoundaryX + iOffBound;
+canvas.height = rBoundaryY + iOffBound;
 var ctx = canvas.getContext("2d");
 ctx.beginPath();
 DrawSquares();
+PlaceObstacles();
 PlaceRobot(xCurrent, yCurrent, sCurrentDir, "Show");
 Report();
 ctx.stroke();
@@ -57,25 +45,101 @@ $(document).ready(function () {
    });
 });
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Clear Obstacles
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+function ClearObstacles()
+{
+   for (iCol = 1; iCol <= iTableTopSize; iCol++) {
+      for (iRow = 1; iRow <= iTableTopSize; iRow++) {
+         Obstacle[iRow][iCol]=0;
+      }
+   }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Place Obstacles
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+function PlaceObstacles()
+{
+   for (iCol = 1; iCol <= iTableTopSize; iCol++) {
+      for (iRow = 1; iRow <= iTableTopSize; iRow++) {
+         if(Obstacle[iRow][iCol] == 1) {
+            var { x, y } = GetXY(iRow, iCol);
+            var width =rSquareSizeX*0.5;
+            var height=rSquareSizeY*0.5;
+            x=x + width*0.5;
+            y=y + height*0.5;
+            ctx.fillStyle = 'red';
+            ctx.fillRect(x,y,width,height);
+         }
+      }
+   }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+// given robots row,col return favourable grid location x, Y
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+function GetXY(iRow, iCol) {
+   var x = (iCol - 1) * rSquareSizeX + iOffBound/2;
+   var y = (iRow - 1) * rSquareSizeY + iOffBound/2;
+   return { x, y };
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+// enable place onstacles/rbot/clear from button
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+function DoPlaceObstacles()
+{
+   clickMode = 'obstacle';
+   document.getElementById("ModeId").innerHTML="Click Square(s) in Grid to Place lots of <u>Obstacle(s).</u>";
+}
+function DoPlaceRobot()
+{
+   clickMode = 'robot';
+   document.getElementById("ModeId").innerHTML="Click a Square in Grid to Place the <u>Robot.</u>";
+}
+function DoClearAll()
+{
+    location.reload();
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 // place robot on any square with mouse
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 function canvasClick(e) {
-var { iRow, iCol } = GetRowCol(e.offsetX, e.offsetY);
-console.log('x,y=' + e.offsetX + ', ' + e.offsetY);
-console.log('iRow,iCol=' + iRow + ', ' + iCol);
-
-   if (iRow > 0 && iCol > 0)  {
-      var x = (iCol - 1) * rSquareSize + rSquareSize / 2;
-      var y = (iRow - 1) * rSquareSize + rSquareSize / 2;
-
-      PlaceRobot(xCurrent, yCurrent, sCurrentDir, "Erase");
-      PlaceRobot(x, y, sCurrentDir, "Show");
-      xCurrent = x;
-      yCurrent = y;
-      BoundaryHit("Clear");
-   } else {
-      BoundaryHit("Outside");
+   var { iRow, iCol } = GetRowCol(e.offsetX, e.offsetY);
+//
+// robot or obstacle already at this location
+//
+   if( Obstacle[iRow][iCol] == 1) {
+      return true;
    }
+   else if( Obstacle[iRow][iCol] == 2) {
+      return true;
+   }
+//
+// place robot
+//
+   if(clickMode == 'robot') {
+      if (iRow > 0 && iCol > 0)  {
+         var x = (iCol - 1) * rSquareSizeX + rSquareSizeX / 2 + iOffBound /2;
+         var y = (iRow - 1) * rSquareSizeY + rSquareSizeY / 2 + iOffBound /2;
+         PlaceRobot(xCurrent, yCurrent, sCurrentDir, "Erase");
+         PlaceRobot(x, y, sCurrentDir, "Show");
+         xCurrent = x;
+         yCurrent = y;
+         BoundaryHit("Clear");
+      } else {
+         BoundaryHit("Outside");
+      }
+   }
+//
+// place obstacle
+//
+   else if(clickMode == 'obstacle') {
+      var { iRow, iCol } = GetRowCol(e.offsetX, e.offsetY);
+      Obstacle[iRow][iCol] = 1;
+      PlaceObstacles();
+   }
+//
+// report
+//
    Report();
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -85,9 +149,10 @@ function Report() {
    var { iRow, iCol } = GetRowCol(xCurrent, yCurrent);
    var sReport = `Report<br>Direction Facing: ${sCurrentDir} <br>X=${xCurrent}, Y=${yCurrent} <br>Row=${iRow}, Col=${iCol}`;
    if (iRow > 0 && iCol > 0) {
-      document.getElementById("ReportId").innerHTML = sReport + "<br>Success!";
+      document.getElementById("ReportId").innerHTML = sReport;
    } else {
-      document.getElementById("ReportId").innerHTML = sReport + "<br>Failure!";
+      document.getElementById("ReportId").innerHTML = sReport;
+      document.getElementById("BoundaryId").innerHTML="<br>Failure!";
    }
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -99,10 +164,10 @@ function GetRowCol(x, y) {
 
    for (i = 1; i <= iTableTopSize; i++) {
       for (j = 1; j <= iTableTopSize; j++) {
-         var xPos = (i - 1) * rSquareSize + 2;
-         var yPos = (j - 1) * rSquareSize + 2;
-         if (x >= xPos && x <= xPos + rSquareSize) {
-            if (y >= yPos && y <= yPos + rSquareSize) {
+         var xPos = (i - 1) * rSquareSizeX + iOffBound/2;
+         var yPos = (j - 1) * rSquareSizeY + iOffBound/2;
+         if (x >= xPos && x <= xPos + rSquareSizeX) {
+            if (y >= yPos && y <= yPos + rSquareSizeY) {
                iCol = i;
                iRow = j;
                i = iTableTopSize;
@@ -121,9 +186,9 @@ function DrawSquares() {
    var i, j;
    for (i = 1; i <= iTableTopSize; i++) {
       for (j = 1; j <= iTableTopSize; j++) {
-         var xPos = (i - 1) * rSquareSize + 2;
-         var yPos = (j - 1) * rSquareSize + 2;
-         ctx.rect(xPos, yPos, rSquareSize, rSquareSize);
+         var xPos = (i - 1) * rSquareSizeX + iOffBound/2;
+         var yPos = (j - 1) * rSquareSizeY + iOffBound/2;
+         ctx.rect(xPos, yPos, rSquareSizeX, rSquareSizeY);
       }
    }
 }
@@ -165,40 +230,72 @@ function MoveLeft() {
 // move robot direction facing 1 square
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 function MoveRobot() {
-   //
-   // erase current position
-   //
+//
+// erase current position
+//
    PlaceRobot(xCurrent, yCurrent, sCurrentDir, "Erase");
-   //
-   // move to new location
-   //
+//
+// move to new location
+//
    var xPos = xCurrent;
    var yPos = yCurrent;
+//
+// heading North
+//
    if (sCurrentDir == "North") {
-      if (yPos - rSquareSize > 0) {
-         yPos = yPos - rSquareSize;
-         BoundaryHit("Clear");
+      if (yPos - rSquareSizeY > 0) {
+         var { iRow, iCol } = GetRowCol(xPos, yPos - rSquareSizeY);
+         if(Obstacle[iRow][iCol] == 1) {
+            BoundaryHit("Obstacle Heading North");
+         } else {
+            yPos = yPos - rSquareSizeY;
+            BoundaryHit("Clear");
+         }
       } else {
          BoundaryHit("North");
       }
+//
+// heading South
+//
    } else if (sCurrentDir == "South") {
-      if (yPos + rSquareSize < rBoundary) {
-         yPos = yPos + rSquareSize;
-         BoundaryHit("Clear");
+      if (yPos + rSquareSizeY < rBoundaryY) {
+         var { iRow, iCol } = GetRowCol(xPos, yPos + rSquareSizeY);
+         if(Obstacle[iRow][iCol] == 1) {
+            BoundaryHit("Obstacle Heading South");
+         } else {
+            yPos = yPos + rSquareSizeY;
+            BoundaryHit("Clear");
+         }
       } else {
          BoundaryHit("South");
       }
+//
+// heading West
+//
    } else if (sCurrentDir == "West") {
-      if (xPos - rSquareSize > 0) {
-         xPos = xPos - rSquareSize;
-         BoundaryHit("Clear");
+      if (xPos - rSquareSizeX > 0) {
+         var { iRow, iCol } = GetRowCol(xPos - rSquareSizeX, yPos);
+         if(Obstacle[iRow][iCol] == 1) {
+            BoundaryHit("Obstacle Heading West");
+         } else {
+            xPos = xPos - rSquareSizeX;
+            BoundaryHit("Clear");
+         }
       } else {
          BoundaryHit("West");
       }
+//
+// heading East
+//
    } else if (sCurrentDir == "East") {
-      if (xPos + rSquareSize < rBoundary) {
-         xPos = xPos + rSquareSize;
-         BoundaryHit("Clear");
+      if (xPos + rSquareSizeX < rBoundaryX) {
+         var { iRow, iCol } = GetRowCol(xPos + rSquareSizeX, yPos);
+         if(Obstacle[iRow][iCol] == 1) {
+            BoundaryHit("Obstacle Heading East");
+         } else {
+            xPos = xPos + rSquareSizeX;
+            BoundaryHit("Clear");
+         }
       } else {
          BoundaryHit("East");
       }
@@ -215,8 +312,10 @@ function MoveRobot() {
 // robot message when hits Wall
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 function BoundaryHit(sDir) {
-   if (sDir == "Clear") {
-      document.getElementById("BoundaryId").innerHTML = "";
+   if(sDir.match(/Obstacle/)) {
+      document.getElementById("BoundaryId").innerHTML = sDir;
+   } else if (sDir == "Clear") {
+      document.getElementById("BoundaryId").innerHTML = "Success!";
    } else if (sDir == "Outside") {
       document.getElementById("BoundaryId").innerHTML = "Location Selected is Outside Boundary!";
    } else {
@@ -227,7 +326,7 @@ function BoundaryHit(sDir) {
 // place robot at specfic location with a specfic direction
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 function PlaceRobot(xIn, yIn, sDir, sMode) {
-   var iOff = rSquareSize / 3;
+   var iOff = Math.min(rSquareSizeX,rSquareSizeY) / 3;
    var iInc = 5;
    var path = new Path2D();
    //
@@ -236,8 +335,14 @@ function PlaceRobot(xIn, yIn, sDir, sMode) {
    ctx.fillStyle = "#0000FF";
    if (sMode == "Erase") {
       ctx.fillStyle = "#FFFFFF";
-      iOff = rSquareSize / 2.8;
+      iOff = Math.min(rSquareSizeX,rSquareSizeY) / 2.70;
       iInc = 6;
+      var { iRow, iCol } = GetRowCol(xCurrent, yCurrent);
+      Obstacle[iRow][iCol]=0;
+   }
+   else if(sMode == 'Show') {
+      var { iRow, iCol } = GetRowCol(xIn, yIn);
+      Obstacle[iRow][iCol]=2;
    }
    //
    // draw
